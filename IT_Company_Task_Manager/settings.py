094db3c728f20c12d6a3838b3b8b6085bf0a03b0
Andrii Omelchenko <andreyomem@gmail.com>
2025-06-30 11:25:05 +0300

    add settings.py AUTH_USER_MODEL, INSTALLED_APPS

--- IT_Company_Task_Manager/settings.py
+++ IT_Company_Task_Manager/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'task_catalog'
 ]
 
 MIDDLEWARE = [
@@ -98,6 +99,8 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+
+AUTH_USER_MODEL = 'task_catalog.Worker'
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
